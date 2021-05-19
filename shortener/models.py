2feaf95fff0ca6396074409bfafa78f9fc6e9f55
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-05-19 22:49:44 +0530

    Create Url Model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    link = models.CharField(max_length=20000)
+    uuid = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"<UUID: {self.uuid}>"
