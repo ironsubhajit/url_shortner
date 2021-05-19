e557cad77dabe17d1acd8871ccca8a1e4d425765
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-05-19 22:50:58 +0530

    Add 'create' view to create model Url

--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import uuid
+
+from .models import Url
 
 
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]  # generate unique id of length 5
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
