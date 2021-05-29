bf64f4dccd6b3dcdddec10e81480ff381ce05118
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-05-29 18:46:15 +0530

    Create filter to short long-link characters for simple view

--- shortener/templatetags/generator.py
+++ shortener/templatetags/generator.py
@@ -8,3 +8,14 @@ register = template.Library()
 def gen(val):
     ''' Generates Serial row no '''
     return next(val)
+
+
+@register.filter
+def genshortlen(val):
+    ''' short long-link characters for simple view '''
+    if val[:8] == 'https://':
+        return f'{val[8:20]}...'
+    elif val[:7] == 'http://':
+        return f'{val[7:19]}...'
+    else:
+        return f'{val[:12]}...'
