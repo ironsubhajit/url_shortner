3ee6fbaa26b4de292163425c5ad18a7c69717308
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-05-29 18:53:15 +0530

    Added Copy to Clipboard function with js

--- static/js/app.js
+++ static/js/app.js
@@ -15,3 +15,64 @@ $(document).on('submit', '#post-form', function(e){
         }
     });
 });
+
+
+// funtions for copy functionality
+function outFunc() {
+  var tooltip = $('.tooltiptext');
+  tooltip.text("click to copy");
+};
+
+function copyToClipboard(link){
+
+  // Create a dummy input to copy the string array inside it
+  var dummy = document.createElement("input");
+
+  // add element before js
+  var main_content = $('.content')[0];
+  main_content.appendChild(dummy);
+  
+  // Set its ID
+  dummy.setAttribute("id", "dummy_id");
+  
+  // Output the var into it
+  document.getElementById("dummy_id").value = link;
+  
+    // Select it
+  dummy.select();
+  
+  // Copy its contents
+  document.execCommand('copy');
+
+  // Remove it as its not needed anymore
+  main_content.removeChild(dummy);
+};
+
+function showCopyStatus(link) {
+  alert(`Link Copied: ${link}`);
+};
+
+
+$('.clicktocopy').click(function() {
+        
+    // var link = $(".shortLink");
+    var row_id = $(this).attr("id");
+    // var row_id = $(event.target)[0].id;
+    row_id = `#${row_id}`;
+
+    // generate link id using row no
+    var link_id = `#link${row_id.slice(-1)}`;
+
+    var link = $(link_id).attr("href");
+
+    copyToClipboard(link);
+
+    // temp sol. for tooltip
+    showCopyStatus(link);
+
+    var tooltip = $('.tooltiptext');
+    tooltip.text("Link Copied!");
+});
+
+
+
