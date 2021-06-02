f88f8687f34b0c834854f75883360dc0c7ee0efe
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-06-02 22:50:17 +0530

    Add About page url

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,6 +4,7 @@ from . import views as shortener_views
 
 urlpatterns = [
     path('', shortener_views.base, name="home"),
+    path('about', shortener_views.about, name="about"),
     path('create', shortener_views.create, name="create"),
     path('go/<str:pk>', shortener_views.go, name="go"),
 ]
