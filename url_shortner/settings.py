e19bb504c4a220ffff33c5bd7b4be2e453fca637
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-06-12 20:06:36 +0530

    Install Accounts App

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shortener.apps.ShortenerConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
