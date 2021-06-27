365a51cbf5d1872b61ef23433afddf50e3491256
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-06-27 17:49:18 +0530

    accounts app javascript added

---
+++ accounts/static/accounts/js/accounts.js
@@ -0,0 +1,10 @@
+function changBrCol () {
+    $('input, select').on('focus', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
+    });
+    $('input, select').on('blur', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
+    });
+};
+
+$(document).ready(changBrCol());
