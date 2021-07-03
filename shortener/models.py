e6d3fc92ef7efa927ae331a5bca873e00c8e13e1
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-07-03 22:44:51 +0530

    Create a Url model for a particular user

--- shortener/models.py
+++ shortener/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Url(models.Model):
@@ -7,3 +11,12 @@ class Url(models.Model):
 
     def __str__(self):
         return f"<UUID: {self.uuid}>"
+
+
+class UserUrl(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.CharField(max_length=20000)
+    uuid = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"<{self.user} Url>"
