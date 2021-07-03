52b53042672475bc5a9a350674a5b3b7249d8fc3
Subhajit Mandal <mandalsubhajit78@gmail.com>
2021-07-03 22:47:52 +0530

    Register 'User url' model to admin

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Url
+from .models import Url, UserUrl
 
 # Register your models here.
 admin.site.register(Url)
+
+
+@admin.register(UserUrl)
+class UserUrlAdmin(admin.ModelAdmin):
+    list_display = ['link', 'uuid', 'user']
